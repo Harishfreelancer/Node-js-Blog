ef37ba4f68d53080db9ffea19d4f3237d01a854c
Harish <jacksonhari25@gmail.com>
2021-01-20 10:29:19 +0530

    Initial changes for room state

--- blog_nodejs/routes/myroom.js
+++ blog_nodejs/routes/myroom.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const posts = [];
+// const posts = [];
+let rooms = {
+    room:"",
+    creatorname:"",
+    posts:[]
+}
 router.get("/",(req,res)=>{
-    let room = req.query.room
-    let name = req.query.name
-    res.render("myroom",{name,room});
+    rooms.room = req.query.room;
+    rooms.creatorname = req.query.name;
+    res.render("myroom",{room:rooms.room,name:rooms.creatorname});
 })
 router.get("/posts",(req,res)=>{
-    res.render("posts",{posts});
+    res.render("posts",{myposts:rooms.posts});
 })
 router.post("/posts",(req,res)=>{
     const post = req.body.post;
-     posts.push(post);
-    
-    res.render("posts",{posts});
+    rooms.posts.push(post);
+    res.render("posts",{ myposts:rooms.posts})
 })
 module.exports = router;
